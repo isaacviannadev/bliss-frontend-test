62a761c8c847038728ad7c5310949fdf45a4d234
isaac <isaacsvianna@gmail.com>
2023-03-10 22:01:02 +0000

    chore: create Loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import { LoadingContainer } from './styled';
+
+const Loading = () => {
+  return (
+    <LoadingContainer>
+      <h1> Loading...</h1>;
+    </LoadingContainer>
+  );
+};
+
+export default Loading;
