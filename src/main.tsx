a38a486d30f81d7c10cf733c9c46816144f8f048
isaac <isaacsvianna@gmail.com>
2023-03-12 20:13:06 +0000

    chore: create routes and add route provider to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/App';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
 import GlobalStyle from './styles/global';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
     <GlobalStyle />
   </React.StrictMode>
 );
