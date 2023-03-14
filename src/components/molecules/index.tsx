f59f3dc010b522cc03bfc18ca35cec6dda37e993
isaac <isaacsvianna@gmail.com>
2023-03-14 14:58:21 +0000

    refactor: add export by index in components context

---
+++ src/components/molecules/index.tsx
@@ -0,0 +1,6 @@
+export * from './HeaderSearch';
+export * from './Modal';
+export * from './ProgressBar';
+export * from './Question';
+export * from './ShareScreenModal';
+export * from './VoteModal';
