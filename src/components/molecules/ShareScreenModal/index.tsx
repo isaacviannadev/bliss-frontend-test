76da4f566753b219cc20bec64afa30da09710eb0
isaac <isaacsvianna@gmail.com>
2023-03-14 04:01:20 +0000

    chore: create specific uses modals

---
+++ src/components/molecules/ShareScreenModal/index.tsx
@@ -0,0 +1,97 @@
+import { EnvelopeSimple, Link, PaperPlaneTilt } from '@phosphor-icons/react';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { validateEmail } from '../../../helpers';
+import Button from '../../ui/Button';
+import Input from '../../ui/Input';
+import Text from '../../ui/Text';
+
+import Modal from '../Modal';
+import { ShareScreenContent, ShareScreenTitle } from './styled';
+
+type ShareScreenModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const ShareScreenModal = ({
+  isOpen = false,
+  onClose,
+}: ShareScreenModalProps) => {
+  const [email, setEmail] = useState<string>('');
+
+  const closeAndClear = () => {
+    setEmail('');
+    onClose();
+  };
+
+  const handleShare = async () => {
+    const url = window.location.href;
+
+    await fetch(
+      `https://private-0672e-blissrecruitmentapi.apiary-mock.com/share?destination_email=${email}&content_url=${url}`,
+      {
+        method: 'POST',
+      }
+    )
+      .then((response) => {
+        if (response.status === 200) {
+          toast.success('Screen shared successfully');
+        } else {
+          toast.error('Something went wrong');
+        }
+      })
+      .catch(() => {
+        toast.error('Something went wrong');
+      })
+      .finally(() => {
+        closeAndClear();
+      });
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      const input = document.getElementById('email') as HTMLInputElement;
+      input.focus();
+    }
+  }, [isOpen]);
+
+  return (
+    <Modal isOpen={isOpen} onClose={closeAndClear}>
+      <ShareScreenContent>
+        <ShareScreenTitle>
+          Share screen <PaperPlaneTilt size={22} />
+        </ShareScreenTitle>
+        <Text>Who do you want to send it to?</Text>
+
+        <Input
+          id='email'
+          icon={<EnvelopeSimple size={22} />}
+          type='email'
+          placeholder='jhondoe@xyz.com'
+          value={email}
+          onChange={({ target: { value } }) => {
+            setEmail(value);
+          }}
+        />
+        <Input
+          id='Url'
+          icon={<Link size={22} />}
+          type='text'
+          defaultValue={window.location.href}
+          readOnly
+        />
+        <Button
+          size='small'
+          variant='primary'
+          disabled={!validateEmail(email)}
+          onClick={handleShare}
+        >
+          Share screen
+        </Button>
+      </ShareScreenContent>
+    </Modal>
+  );
+};
+
+export default ShareScreenModal;
---
+++ src/components/molecules/ShareScreenModal/styled.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const ShareScreenContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+  gap: 1rem;
+`;
+
+export const ShareScreenTitle = styled.h2`
+  text-align: center;
+  font-size: 2.4rem;
+  font-weight: 600;
+  color: #141414;
+
+  svg {
+    fill: #57b194;
+  }
+`;
