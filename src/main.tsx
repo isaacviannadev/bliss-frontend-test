3e2368b63d1b852d045ca2230ceaf7d9b171437f
isaac <isaacsvianna@gmail.com>
2023-03-14 04:00:27 +0000

    chore: add toaster provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import GlobalStyle from './styles/global';
@@ -8,5 +9,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
     <GlobalStyle />
+    <Toaster position='top-right' reverseOrder={false} />
   </React.StrictMode>
 );
